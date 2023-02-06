d4db83a54f87db50aedeb29f1510b11234b55c6f
N0tAdm1n <n0tadm1n29@gmail.com>
2023-02-06 20:34:51 +0530

    Add id parameter and property

--- src/js/shipFactory.js
+++ src/js/shipFactory.js
@@ -1,4 +1,5 @@
-function ship(length) {
+function ship(length, orientation = 0, id) {
+  // 0 for horizonatal, 1 for vertcal
   let _hitCount = 0;
 
   function hit() {
@@ -7,11 +8,19 @@ function ship(length) {
   function isSunk() {
     return _hitCount >= length;
   }
+  function changeOrientation() {
+    if (orientation == 0) orientation = 1;
+    else orientation = 0;
+  }
   return {
     length,
+    orientation,
+    id,
     hit,
     isSunk,
+    changeOrientation,
   };
 }
 
-export default ship;
+// export default ship;
+module.exports = { ship };
