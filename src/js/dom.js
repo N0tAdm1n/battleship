b6ec4e26c682aef2ab1a9654e47b210bdaa24956
N0tAdm1n <n0tadm1n29@gmail.com>
2023-02-09 18:56:58 +0530

    Add createGameboards()

---
+++ src/js/dom.js
@@ -0,0 +1,23 @@
+function createGameboards() {
+  const gameboards = document.querySelectorAll(".gameboard");
+
+  gameboards.forEach((board) => makegrid(board));
+}
+
+function makegrid(board) {
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 10; j++) {
+      const block = document.createElement("div");
+      block.classList.add("block");
+
+      row.append(block);
+    }
+
+    board.append(row);
+  }
+}
+
+module.exports = { createGameboards };
